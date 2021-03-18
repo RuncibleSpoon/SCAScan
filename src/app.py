ab754579b01cf9803fe80d17ac53c7a1e0046e05
RuncibleSpoon <robert.haynes@gmail.com>
2021-03-18 14:43:52 -0700

    Update app.py
    fixing error checking

--- src/app.py
+++ src/app.py
@@ -6,10 +6,13 @@ import requests
 
 def check_ip():
     r = requests.get("https://api.myip.com/")
-    current_ip = r.json()['ip']
+    if r:
+        current_ip = r.json()['ip']
 
-    message = f"{current_ip}"
-    return current_ip
+        message = f"{current_ip}"
+        return current_ip
+    else:
+    	return "failed"
 
 if __name__ == '__main__':
     current_ip = check_ip()
